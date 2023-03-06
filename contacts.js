6e93424a95ca06b435e8f613869b2a2719a0f780
Anton Kovsh <kovsh_anton@ukr.net>
2023-03-06 22:02:20 +0200

    upd

---
+++ contacts.js
@@ -0,0 +1,61 @@
+const { v4 } = require("uuid");
+
+const path = require("path");
+const fs = require("fs").promises;
+
+const contactsPath = path.join("db", "contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+
+    return contacts;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const getContact = contacts.find((contact) => contact.id === contactId);
+
+    return getContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const removeContact = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(removeContact));
+    return removeContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = { name, email, phone, id: v4() };
+    contacts.push(newContact);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
